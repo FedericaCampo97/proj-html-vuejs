1310a8c2a777746b2791a6c58f405102f284291e
FedericaCampo97 <federica.campo97@gmail.com>
2023-10-03 21:43:25 +0200

    End first block main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,26 +3,59 @@
 
 <template>
     <div class="block_moment">
-        <div class="col-8">
-            <div class="p-5">
-                <h1>EVERY MOMENT COUNTS</h1>
-                <div class="line"></div>
-                <p>Curabitur ipsum dolor, sit amet consectetur adipisicing elit. Dolore repellendus commodi repellat, animi
-                    porro,
-                    voluptatem amet veniam reprehenderit modi incidunt asperiores officiis dolores.</p>
-                <p class="text_2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus fugiat officiis
-                    soluta, dolor illum
-                    repudiandae inventore, quisquam, culpa nostrum ratione consequuntur doloremque molestiae numquam facere
-                    illo. Saepe, tempora unde. Consequatur. Culpa nostrum ratione consequuntur.</p>
-                <p class="my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ipsam voluptatem fugit sunt
-                    excepturi
-                    accusamus. Tempore tenetur explicabo necessitatibus.</p>
-                <button>OUR MISSION</button>
-
-
+        <div class="d-flex">
+            <div class="col-8 p-5">
+                <div class="p-5 ms-4">
+                    <h1>EVERY MOMENT COUNTS</h1>
+                    <div class="line"></div>
+                    <p>Curabitur ipsum dolor, sit amet consectetur adipisicing elit. Dolore repellendus commodi repellat,
+                        animi
+                        porro,
+                        voluptatem amet veniam reprehenderit modi incidunt asperiores officiis dolores.</p>
+                    <p class="text_2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus fugiat officiis
+                        soluta, dolor illum
+                        repudiandae inventore, quisquam, culpa nostrum ratione consequuntur doloremque molestiae numquam
+                        facere
+                        illo. Saepe, tempora unde. Consequatur. Culpa nostrum ratione consequuntur.</p>
+                    <p class="my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ipsam voluptatem fugit sunt
+                        excepturi
+                        accusamus. Tempore tenetur explicabo necessitatibus.</p>
+                    <button>OUR MISSION</button>
+                </div>
             </div>
+            <div class="col-4 number_person p-5">
+                <div class="d-flex align-items-center justify-content-center flex-column">
+                    <div class="number_volontareers">
+                        <h1 class="text-white"><svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                                viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path
+                                    d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />
+                            </svg> 2032</h1>
+                        <p class="text-white">Volunteers worldwide</p>
+                    </div>
+                    <div class="number_project">
+                        <h1 class="text-white"><svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                                viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path
+                                    d="M57.7 193l9.4 16.4c8.3 14.5 21.9 25.2 38 29.8L163 255.7c17.2 4.9 29 20.6 29 38.5v39.9c0 11 6.2 21 16 25.9s16 14.9 16 25.9v39c0 15.6 14.9 26.9 29.9 22.6c16.1-4.6 28.6-17.5 32.7-33.8l2.8-11.2c4.2-16.9 15.2-31.4 30.3-40l8.1-4.6c15-8.5 24.2-24.5 24.2-41.7v-8.3c0-12.7-5.1-24.9-14.1-33.9l-3.9-3.9c-9-9-21.2-14.1-33.9-14.1H257c-11.1 0-22.1-2.9-31.8-8.4l-34.5-19.7c-4.3-2.5-7.6-6.5-9.2-11.2c-3.2-9.6 1.1-20 10.2-24.5l5.9-3c6.6-3.3 14.3-3.9 21.3-1.5l23.2 7.7c8.2 2.7 17.2-.4 21.9-7.5c4.7-7 4.2-16.3-1.2-22.8l-13.6-16.3c-10-12-9.9-29.5 .3-41.3l15.7-18.3c8.8-10.3 10.2-25 3.5-36.7l-2.4-4.2c-3.5-.2-6.9-.3-10.4-.3C163.1 48 84.4 108.9 57.7 193zM464 256c0-36.8-9.6-71.4-26.4-101.5L412 164.8c-15.7 6.3-23.8 23.8-18.5 39.8l16.9 50.7c3.5 10.4 12 18.3 22.6 20.9l29.1 7.3c1.2-9 1.8-18.2 1.8-27.5zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z" />
+                            </svg>
+                            132</h1>
+                        <p class="text-white">Active projects</p>
+                    </div>
+                    <div class="number_donation">
+                        <h1 class="text-white"><svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                                viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path
+                                    d="M160 0c17.7 0 32 14.3 32 32V67.7c1.6 .2 3.1 .4 4.7 .7c.4 .1 .7 .1 1.1 .2l48 8.8c17.4 3.2 28.9 19.9 25.7 37.2s-19.9 28.9-37.2 25.7l-47.5-8.7c-31.3-4.6-58.9-1.5-78.3 6.2s-27.2 18.3-29 28.1c-2 10.7-.5 16.7 1.2 20.4c1.8 3.9 5.5 8.3 12.8 13.2c16.3 10.7 41.3 17.7 73.7 26.3l2.9 .8c28.6 7.6 63.6 16.8 89.6 33.8c14.2 9.3 27.6 21.9 35.9 39.5c8.5 17.9 10.3 37.9 6.4 59.2c-6.9 38-33.1 63.4-65.6 76.7c-13.7 5.6-28.6 9.2-44.4 11V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V445.1c-.4-.1-.9-.1-1.3-.2l-.2 0 0 0c-24.4-3.8-64.5-14.3-91.5-26.3c-16.1-7.2-23.4-26.1-16.2-42.2s26.1-23.4 42.2-16.2c20.9 9.3 55.3 18.5 75.2 21.6c31.9 4.7 58.2 2 76-5.3c16.9-6.9 24.6-16.9 26.8-28.9c1.9-10.6 .4-16.7-1.3-20.4c-1.9-4-5.6-8.4-13-13.3c-16.4-10.7-41.5-17.7-74-26.3l-2.8-.7 0 0C119.4 279.3 84.4 270 58.4 253c-14.2-9.3-27.5-22-35.8-39.6c-8.4-17.9-10.1-37.9-6.1-59.2C23.7 116 52.3 91.2 84.8 78.3c13.3-5.3 27.9-8.9 43.2-11V32c0-17.7 14.3-32 32-32z" />
+                            </svg>
+                            3,8M</h1>
+                        <p class="text-white">Donated</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <div class="col-4"></div>
+
 
     </div>
 </template>
