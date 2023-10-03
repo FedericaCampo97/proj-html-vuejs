c25c9c3035ae1271da500b09ca95c68a3e476bce
FedericaCampo97 <federica.campo97@gmail.com>
2023-10-03 16:38:28 +0200

    Initial main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,3 +1,31 @@
 <script></script>
-<template></template>
+
+
+<template>
+    <div class="block_moment">
+        <div class="col-8">
+            <div class="p-5">
+                <h1>EVERY MOMENT COUNTS</h1>
+                <div class="line"></div>
+                <p>Curabitur ipsum dolor, sit amet consectetur adipisicing elit. Dolore repellendus commodi repellat, animi
+                    porro,
+                    voluptatem amet veniam reprehenderit modi incidunt asperiores officiis dolores.</p>
+                <p class="text_2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus fugiat officiis
+                    soluta, dolor illum
+                    repudiandae inventore, quisquam, culpa nostrum ratione consequuntur doloremque molestiae numquam facere
+                    illo. Saepe, tempora unde. Consequatur. Culpa nostrum ratione consequuntur.</p>
+                <p class="my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ipsam voluptatem fugit sunt
+                    excepturi
+                    accusamus. Tempore tenetur explicabo necessitatibus.</p>
+                <button>OUR MISSION</button>
+
+
+            </div>
+        </div>
+        <div class="col-4"></div>
+
+    </div>
+</template>
+
+
 <style></style>
